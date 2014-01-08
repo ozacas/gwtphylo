aae538b624f6a451f94d114a3bf53049da32f5c0
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@4bc382aa-05cf-c2ce-85bc-c7e39315bd93>
2014-01-08 03:29:05 +0000

    more work... just need to figure out how to get jsphylosvg to do something...

--- au.edu.unimelb.plantcell.gwtphylo/src/au/edu/unimelb/plantcell/gwtphylo/client/PhyloXMLService.java
+++ au.edu.unimelb.plantcell.gwtphylo/src/au/edu/unimelb/plantcell/gwtphylo/client/PhyloXMLService.java
@@ -9,4 +9,24 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 @RemoteServiceRelativePath("PhyloXMLServiceImpl")
 public interface PhyloXMLService extends RemoteService {
 	String[] getSuperfamilies() throws IllegalArgumentException;
+	
+	/**
+	 * Returns all sub-categories (separate tree analyses) for the given superfamily. May vary from family to family.
+	 * @param superfamily
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	String[] getCategories(final String superfamily) throws IllegalArgumentException;
+	
+	/**
+	 * Returns the list of available trees for the given category (ie. analysis) of a particular superfamily
+	 */
+	String[] getTrees(String superfamily, String category) throws IllegalArgumentException;
+	
+	/**
+	 * Returns the phyloxml associated with the given selection
+	 */
+	String getPhyloXML(String superfamily, String category, String name) throws IllegalArgumentException;
+	
+	
 }
