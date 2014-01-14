ba1c7ef663816c06ffde4deaf832e419a7cc99ff
pcbrc-enquiries@unimelb.edu.au <pcbrc-enquiries@unimelb.edu.au@4bc382aa-05cf-c2ce-85bc-c7e39315bd93>
2014-01-14 20:53:16 +0000

    Refactoring to cleanup code and phyloxml tree items are now "cleaned up" to be shorter and more presentable to the user on the webpage

--- au.edu.unimelb.plantcell.gwtphylo/src/au/edu/unimelb/plantcell/gwtphylo/client/TreeItemUtils.java
+++ au.edu.unimelb.plantcell.gwtphylo/src/au/edu/unimelb/plantcell/gwtphylo/client/TreeItemUtils.java
@@ -2,7 +2,14 @@ package au.edu.unimelb.plantcell.gwtphylo.client;
 
 import com.google.gwt.user.client.ui.TreeItem;
 
+/**
+ * A collection of static methods for evaluating selected tree items rather than scattering the logic throughout the system
+ * 
+ * @author http://www.plantcell.unimelb.edu.au/bioinformatics
+ *
+ */
 public class TreeItemUtils {
+	
 	public static boolean isSuperFamilyTreeItem(final TreeItem it) {
 		if (it == null)
 			return false;
